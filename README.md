9bdfda687decacb0caec18f901ca6f1a11f45ab5
Kosta-Stajic <kostastajic@gmail.com>
2023-05-31 18:10:30 +0200

    Add rps ui as well as javascript logic to play the game using button.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # rock-paper-scissors
+
+
+Add buttons to the HTML. DONE
+
+Make sure each button corresponds to rock/paper/scissors player choice. MAIN QUESTION *//How to add playerselction to different clicks//* DONE
+
+Clicking on the button has to make the player choice happen DONE
+
+at the same time clicking on the button has to get random computer choice. DONE
+
+PlayRound function comes into play and determines the outcome. DONE
+
+Prints out the the text into a div, instead of a console.log. DONE
+
+Keep track of the score and end the game once the first player reaches 5!. NOT DONE
+
--- script.js
+++ script.js
@@ -1,35 +1,92 @@
 
+
+const button1 = document.querySelector("#rock")
+
+const button2 = document.querySelector("#paper")
+
+const button3 = document.querySelector("#scissors")
+
 const computerChoice = ["rock", "paper", "scissors"];
 
+const body = document.querySelector("body")
+
+const p = document.querySelector("#results")
+body.appendChild(p)
+
+//Take a random choice for the computer//
 function getRandomChoice() {
     return computerChoice[Math.floor(Math.random() * computerChoice.length)];
 }
 
 
-let playerScore = 0
 
+let playerScore = 0
 let computerScore = 0
 
+
+//buttons for playing//
+button1.addEventListener("click", () => {
+    const computerSelection = getRandomChoice()
+    const playerSelection = "rock"
+    playRound(playerSelection, computerSelection)
+    console.log(playerSelection)
+    console.log(computerSelection)
+});
+button2.addEventListener("click", () => {
+    const computerSelection = getRandomChoice()
+    const playerSelection = "paper"
+    playRound(playerSelection, computerSelection)
+    console.log(playerSelection)
+    console.log(computerSelection)
+});
+button3.addEventListener("click", () => {
+    const computerSelection = getRandomChoice()
+    const playerSelection = "scissors"
+    playRound(playerSelection, computerSelection)
+    console.log(playerSelection)
+    console.log(computerSelection)
+});
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("You win, rock beats scissors!")
-        playerScore++
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You win, scissors beats paper!")
-        playerScore++
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You win, paper beats rock!")
-        playerScore++
-    } else if (playerSelection === computerSelection) {
-        console.log("It's a tie!")
-    } else {
-        console.log(`You lose, ${computerSelection} beats ${playerSelection}`)
-        computerScore++
+    if (playerScore === 5 || computerScore === 5) {
+        return;
     }
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++
+        //Figure out how to select the div and feed it the text form the console.log//
+
+        p.textContent = `You win rock beats scissors!`+ " " + `the score is ${playerScore} - ${computerScore}`
+
+
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++
+        p.textContent = `You win, scissors beats paper! `+ " " + `the score is ${playerScore} - ${computerScore}`
+
+
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++
+        p.textContent = `You win, paper beats rock!`+ " " + `the score is ${playerScore} - ${computerScore}`
+
+
+    } else if (playerSelection === computerSelection) {
+        p.textContent = (`It's a tie!`)+ " " + `the score is ${playerScore} - ${computerScore}`
+
+    } else {
+        computerScore++
+        p.textContent = (`You lose, ${computerSelection} beats ${playerSelection}`) + " " + `the score is ${playerScore} - ${computerScore}`
+
+    } if (playerScore === 5 && computerScore < 5) {
+        p.textContent = `Congratulations, you win the game! The score was ${playerScore} to ${computerScore}! Refresh the page to play again!`
+    } else if (playerScore < 5 && computerScore === 5) {
+        p.textContent = `Too bad, you lose the game! The score was ${playerScore} to ${computerScore}! Refresh the page to play again!`
+    } 
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
+document.getElementById("results").textContent = playRound();
+
+
+/* function game() {
+    
         const playerSelection = prompt("Choose paper rock or scissors!").toLowerCase();
         const computerSelection = getRandomChoice()
         playRound(playerSelection, computerSelection)
@@ -40,7 +97,6 @@ function game() {
         console.log(`It's a tie!! the score is ${computerScore} to ${playerScore}`)
     } else {
         console.log(`You lose!! the score is ${computerScore} to ${playerScore}`)
-    }
-}
+    } 
 
-game()
+game()*/
